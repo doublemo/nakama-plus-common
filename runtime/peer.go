1f6f3138665aae98ebc22d687210bf545532f187
RandyMa <randy.msn@hotmail.com>
2024-02-22 19:11:17 +0800

    bool

--- runtime/peer.go
+++ runtime/peer.go
@@ -9,7 +9,7 @@ import (
 type (
 	Peer interface {
 		Send(node string, msg *rtapi.NakamaPeer_Frame, reliable bool) error
-		Broadcast(msg *rtapi.NakamaPeer_Frame, reliable bool, includeSelf bool)
+		Broadcast(msg *rtapi.NakamaPeer_Frame, reliable, includeSelf bool)
 		Call(ctx context.Context, node string, msg *rtapi.NakamaPeer_Frame) (*rtapi.NakamaPeer_Frame, error)
 		SessionToken(tk string) (userID, username string, vars map[string]string, exp int64, online bool, err error)
 	}
