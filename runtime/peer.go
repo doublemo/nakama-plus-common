e649a527f0b0cd194699d451a19654fb9c3e2674
RandyMa <randy.msn@hotmail.com>
2024-02-22 19:07:31 +0800

    includeSelf bool

--- runtime/peer.go
+++ runtime/peer.go
@@ -9,7 +9,7 @@ import (
 type (
 	Peer interface {
 		Send(node string, msg *rtapi.NakamaPeer_Frame, reliable bool) error
-		Broadcast(msg *rtapi.NakamaPeer_Frame, reliable bool)
+		Broadcast(msg *rtapi.NakamaPeer_Frame, reliable bool, includeSelf bool)
 		Call(ctx context.Context, node string, msg *rtapi.NakamaPeer_Frame) (*rtapi.NakamaPeer_Frame, error)
 		SessionToken(tk string) (userID, username string, vars map[string]string, exp int64, online bool, err error)
 	}
